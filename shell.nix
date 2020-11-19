4eca8c1ebe8dd7c5cfd2dda17a768dac56931ee0
Derrick Weis <derrick@derrickweis.com>
2020-11-19 00:53:46 -0800

    remove some unused aliases, add dircolors

--- shell.nix
+++ shell.nix
@@ -13,13 +13,15 @@
       TERM="xterm-256color"
     '';
     shellAliases = {
-      fonts = "fc-list | cut -f2 -d: | sort -u";
-      nix-search = "nix-env -qaP '*' | grep";
-      nix-cleanup = "nix-collect-garbage -d && nix-store --optimize";
+      ll = "ls -lh";
     };
     initExtra = ''
       if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
       unsetopt BEEP
     '';
+  };
+
+  programs.dircolors = {
+    enable = true;
   };
 }
