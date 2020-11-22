1f3f95d04b0cf8df2a7d2900d876c804d579ab40
Derrick Weis <derrick@derrickweis.com>
2020-11-22 15:26:51 -0800

    improve tmux config

--- core.nix
+++ core.nix
@@ -27,5 +27,18 @@
     enable = true;
     keyMode = "vi";
     terminal = "tmux-256color";
+    escapeTime = 0;
+    extraConfig = ''
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
+      bind-key H resize-pane -L 5
+      bind-key J resize-pane -D 5
+      bind-key K resize-pane -U 5
+      bind-key L resize-pane -R 5
+
+      bind-key C-b last-pane
+    '';
   };
 }
