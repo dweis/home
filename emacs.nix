28a8d0a8ef405d696526179704be069c9e728269
Derrick Weis <derrick@derrickweis.com>
2020-11-22 20:42:34 -0800

    more work on emacs setup

--- emacs.nix
+++ emacs.nix
@@ -1,21 +1,36 @@
 { config, lib, pkgs, ... }:
 
 {
+  home.packages = with pkgs; [
+    metals
+  ];
+
   programs.emacs = {
     enable = true;
     extraPackages = (epkgs: with epkgs; [
+      use-package
       # Evil mode
       evil
       evil-leader
       # Themes
       powerline
       airline-themes
-      zenburn-theme
       # Language support
+      flycheck
+      yasnippet
+      posframe
+      dap-mode
+      company
+      company-lsp
       haskell-mode
+      lsp-mode
+      lsp-ui
+      lsp-haskell
       js2-mode
       nix-mode
       scala-mode
+      sbt-mode
+      lsp-metals
     ]);
   };
 
--- emacs/init.el
+++ emacs/init.el
@@ -1,32 +1,104 @@
+(require 'package)
+(package-initialize)
+
+(require 'use-package)
+
 (menu-bar-mode -1)
 (toggle-scroll-bar -1)
 (tool-bar-mode -1)
 
-(setq indent-tabs-mode nil)
-(setq default-tab-width 2)
+;; Enable defer and ensure by default for use-package
+;; Keep auto-save/backup files separate from source code:  https://github.com/scalameta/metals/issues/1027
+(setq use-package-always-defer t
+      use-package-always-ensure t
+      backup-directory-alist `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-(setq inhibit-startup-message t) 
-(setq initial-scratch-message nil)
+(setq indent-tabs-mode nil
+      default-tab-width 2
+      inhibit-startup-message t
+      initial-scratch-message nil)
 
-(require 'evil)
-(evil-mode 1)
+(global-hl-line-mode t)
 
-(require 'evil-leader)
-(global-evil-leader-mode)
-(evil-leader/set-leader "\\")
-(evil-leader/set-key
-  "b" 'switch-to-buffer
-  "w" 'save-buffer)
+(use-package evil
+  :defer .1
+  :config
+  (use-package evil-leader
+    :defer .1
+    :config (global-evil-leader-mode)
+    (evil-leader/set-leader "\\")
+    (evil-leader/set-key
+      "b" 'switch-to-buffer
+      "w" 'save-buffer))
+  (evil-mode))
 
-(require 'zenburn-theme)
 
-(load-theme 'zenburn t)
+(load-theme 'wombat t)
 
-(require 'powerline)
-(require 'airline-themes)
-(load-theme 'airline-zenburn t)
+(use-package powerline
+  :defer .1
+  :config
+  (use-package  airline-themes
+    :defer .1
+    :config (load-theme 'airline-wombat t)))
 
-(require 'nix-mode)
-(require 'haskell-mode)
-(require 'scala-mode)
-(require 'js2-mode)
+(use-package nix-mode)
+(use-package haskell-mode)
+(use-package js2-mode)
+
+(use-package lsp-haskell)
+;; Hooks so haskell and literate haskell major modes trigger LSP setup
+(add-hook 'haskell-mode-hook #'lsp)
+(add-hook 'haskell-literate-mode-hook #'lsp)
+
+;; Enable scala-mode for highlighting, indentation and motion commands
+(use-package scala-mode
+  :interpreter
+  ("scala" . scala-mode))
+
+;; Enable sbt mode for executing sbt commands
+(use-package sbt-mode
+  :commands sbt-start sbt-command
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map)
+  ;; sbt-supershell kills sbt-mode:  https://github.com/hvesalai/emacs-sbt-mode/issues/152
+  (setq sbt:program-options '("-Dsbt.supershell=false")))
+
+;; Enable nice rendering of diagnostics like compile errors.
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package lsp-mode
+  ;; Optional - enable lsp-mode automatically in scala files
+  :hook  (scala-mode . lsp)
+  (lsp-mode . lsp-lens-mode)
+  :config (setq lsp-prefer-flymake nil))
+
+;; Add metals backend for lsp-mode
+(use-package lsp-metals
+  :config (setq lsp-metals-treeview-show-when-views-received t))
+
+;; Enable nice rendering of documentation on hover
+(use-package lsp-ui)
+
+;; lsp-mode supports snippets, but in order for them to work you need to use yasnippet
+;; If you don't want to use snippets set lsp-enable-snippet to nil in your lsp-mode settings
+;;   to avoid odd behavior with snippets and indentation
+(use-package yasnippet
+  :init (yas-global-mode 1))
+
+(use-package company-lsp)
+
+;; Use the Debug Adapter Protocol for running tests and debugging
+(use-package posframe
+  ;; Posframe is a pop-up tool that must be manually installed for dap-mode
+  )
+(use-package dap-mode
+  :hook (lsp-mode . dap-mode)
+  (lsp-mode . dap-ui-mode))
